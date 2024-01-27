5c66e125f639a7e7067a9ea42276943351b8aed2
Abhijith Babu <abijithb2002@gmail.com>
2024-01-27 22:28:21 +0530

    header section completed

--- app/index.js
+++ app/index.js
@@ -1,11 +1,31 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { SafeAreaView, ScrollView, Text, View } from 'react-native'
+import { COLORS, SIZES, images, icons } from '../constants'
+import { Stack } from 'expo-router'
+import { Nearbyjobs, NearbyJobCard, Popularjobs, Welcome, ScreenHeaderBtn } from '../components'
 
 const Home = () => {
   return (
-    <View>
-<Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen options={{
+        headerStyle: { backgroundColor: COLORS.lightWhite },
+        headerShadowVisible: false,
+        headerLeft: () => (
+          <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />
+        ),
+        headerRight: () => (
+          <ScreenHeaderBtn iconUrl={images.profile} dimension='100%' />
+        ),
+        headerTitle: "Bhasi App"
+      }} />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+<Welcome/>
+{/* <Popularjobs/> */}
+{/* <Nearbyjobs/> */}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
